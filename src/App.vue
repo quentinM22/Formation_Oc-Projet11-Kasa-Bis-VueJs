1ae74535ba78fdb570a6439b6d5139c38b75e29e
quentinM22 <quentin.meunier1@laposte.net>
2023-04-16 15:28:03 +0200

    feat: 'App'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-  
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-  </header>
 
   <RouterView />
 </template>
