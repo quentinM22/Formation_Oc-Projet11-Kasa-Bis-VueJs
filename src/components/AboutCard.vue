db4e3b6d018b1c8a0c3e970b265a4c2ce0dc2b49
quentinM22 <quentin.meunier1@laposte.net>
2023-04-16 16:14:46 +0200

    feat: 'View About'

---
+++ src/components/AboutCard.vue
@@ -0,0 +1,70 @@
+<script>
+export default{
+    props:{
+        title: String,
+      description: String,
+      list: Array
+    },
+    data(){
+        return{
+            isOpen: false,
+        }
+    },
+    methods:{
+        toggle(){
+            this.isOpen = !this.isOpen;
+        }
+    }
+}
+</script>
+<template>
+
+			<div className="cmp-AboutCard-btn" v-on:click="toggle()">
+				{{title}}
+				<i class="fa-solid" :class="{'fa-chevron-up': isOpen, 'fa-chevron-down': !isOpen}"></i>
+			</div>
+
+            <div v-if="isOpen" class="cmp-AboutCard-description">
+         <p v-if="description != null">{{ description }}</p>
+         <ul v-else>
+            <li v-for="item, index in list" :key="index">{{ item }}</li>
+         </ul>
+         </div>
+</template>
+<style>
+    .cmp-about-card-container {
+	margin: 0 50px 0;
+}
+.cmp-AboutCard-btn {
+	position: relative;
+	display: flex;
+	justify-content: space-between;
+	background-color: #ff6060;
+	color: white;
+	border-radius: 10px;
+	padding: 10px;
+	margin: 20px 0 0;
+	z-index: 1;
+}
+.cmp-AboutCard-description {
+	position: relative;
+	background-color: #f6f6f6;
+	border-bottom-left-radius: 5px;
+	border-bottom-right-radius: 5px;
+	padding: 40px 15px 20px;
+	margin-top: -10px;
+	font-weight: 400;
+	z-index: 0;
+}
+
+.cmp-AboutCard-description > li {
+	list-style: none;
+	padding: 0 5px 5px;
+}
+@media screen and (max-width: 767px) {
+	.cmp-about-card-container {
+		margin: 0;
+	}
+}
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,25 @@
+<script setup>
+import Header from '../components/Header.vue';
+import Banner from '../components/Banner.vue';
+import AboutCard from '../components/AboutCard.vue';
+import Footer from '../components/Footer.vue';
+
+import Data from '../data/about.json'
+
+</script>
+
 <template>
-  
+    <div className="cmp-body">
+        <Header />
+        <Banner />
+        <div className="cmp-about-card-container">
+                <AboutCard v-for="data, index in Data"
+                 :key="index"
+                 :title="data.title"
+                 :description="data.description" />
+            </div>
+        </div>
+        
+ 
+ <Footer />
 </template>
-
-<style>
-
-</style>
